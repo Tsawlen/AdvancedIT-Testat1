adbe9656c6acf327bd4fb8a4894aed4bf69e460d
Tsawlen <78802953+Tsawlen@users.noreply.github.com>
2021-12-27 11:38:53 +0100

    Comments for Task B

--- src/de/tsawlen/advancedIT/testat/PrivateSharedSpace.java
+++ src/de/tsawlen/advancedIT/testat/PrivateSharedSpace.java
@@ -9,6 +9,7 @@ public class PrivateSharedSpace {
 	private int lastDrivenThrough;
 	private Semaphore[] privateSemaphore;
 	
+	//Initialisiere PrivateSharedSpace
 	public PrivateSharedSpace() {
 		
 		blocked = false;
@@ -22,32 +23,48 @@ public class PrivateSharedSpace {
 		
 	}
 	
+	/***
+	 * This function is responsable for letting Lok 0 enter the shared Space
+	 * Return: void
+	 */
+	
 	public void enterLok0() {
 		
+		//Check if the shared space is free and if lok 1 has already passed through the shared space
 		if(!blocked && lastDrivenThrough == 1) {
+			//Allow passage of the shared space
 			privateSemaphore[0].release();
 		}else {
+			//Wait for the system to allow access to the shared space
 			waiting[0] = true;
 		}
 		
 		try {
 			privateSemaphore[0].acquire();
+			waiting[0] = false;
 			
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
 	
+	/***
+	 * This function is responsable for letting Lok 1 enter the shared Space
+	 * Return: void
+	 */
 	public void enterLok1() {
-		
+		//Check if the shared space is free and if lok 0 has already passed through the shared space
 		if(!blocked && lastDrivenThrough == 0) {
+			//Allow passage of the shared space
 			privateSemaphore[1].release();
 		}else {
+			//Wait for the system to allow access to the shared space
 			waiting[1] = true;
 		}
 		
 		try {
 			privateSemaphore[1].acquire();
+			waiting[1] = false;
 			
 		} catch (InterruptedException e) {
 			e.printStackTrace();
@@ -55,18 +72,30 @@ public class PrivateSharedSpace {
 		
 	}
 	
+	/***
+	 * This function is responsable for letting Lok 0 leave the shared Space
+	 * Return: void
+	 */
 	public void exitLok0() {
 		System.out.println("\t\t\tZug 0 verlässt gemeinsamen Bereich!");
+		//Set the status of the last driven Train to Lok 0
 		lastDrivenThrough = 0;
+		//When Lok 1 is already waiting to access the shared space, let it drive through
 		if(waiting[1]) {
 			privateSemaphore[1].release();
 		}
 		
 	}
 	
+	/***
+	 * This function is responsable for letting Lok 1 leave the shared Space
+	 * Return: void
+	 */
 	public void exitLok1() {
 		System.out.println("\t\t\tZug 1 verlässt gemeinsamen Bereich!");
+		//Set the status of the last driven Train to Lok 1
 		lastDrivenThrough = 1;
+		//When Lok 0 is already waiting to access the shared space, let it drive through
 		if(waiting[0]) {
 			privateSemaphore[0].release();
 		}
--- src/de/tsawlen/advancedIT/testat/PrivateTrainRunner.java
+++ src/de/tsawlen/advancedIT/testat/PrivateTrainRunner.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 
 public class PrivateTrainRunner extends Thread {
 	
+	
 	public static PrivateSharedSpace pShSp = new PrivateSharedSpace();
 	
+	//Settings for the Test
 	private static int velocityTrain1 = 200;
 	private static int velocityTrain2 = 100;
 	private static int lengthWithoutSharedSpace = 400;
@@ -17,21 +19,30 @@ public class PrivateTrainRunner extends Thread {
 		this.id = id;
 	}
 	
+	/***
+	 * This function is responsable for running the threads of the PrivateTrainRunner
+	 */
 	@Override
 	public void run() {
 		
+		//Area for Thread with id 0
 		if(id == 0) {
+			
 			
 			while(true) {
 				
 				System.out.println("Zug 0 möchte in den gemeinsamen Bereich einfahren");
+				//Try enter the shared space
 				pShSp.enterLok0();
 				System.out.println("\t\t\tZug 0 fährt durch gemeinsamen Bereich!");
 				
 				try {
+					//drive through the shared space (simulated through sleeping for the time the train would take to pass trough the shared space)
 					Thread.sleep((length / velocityTrain1) * 1000);
+					//exit the shared space
 					pShSp.exitLok0();
 					System.out.println("Zug 0 fährt auf seinem eigenen Bereich!");
+					//drive through the own space (simulated through sleeping for the time the train would take to pass trough the own space)
 					Thread.sleep((lengthWithoutSharedSpace / velocityTrain1) * 1000);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
@@ -41,7 +52,9 @@ public class PrivateTrainRunner extends Thread {
 			}
 			
 			
-		}else if(id == 1) {
+		}
+		//Area for Thread with id 1
+		else if(id == 1) {
 			
 			while(true) {
 				
@@ -49,9 +62,12 @@ public class PrivateTrainRunner extends Thread {
 				pShSp.enterLok1();
 				System.out.println("\t\t\tZug 1 fährt durch gemeinsamen Bereich!");
 				try {
+					//drive through the shared space (simulated through sleeping for the time the train would take to pass trough the shared space)
 					Thread.sleep((length / velocityTrain2) * 1000);
+					//exit the shared space
 					pShSp.exitLok1();
 					System.out.println("\t\t\t\t\t\tZug 1 fährt auf seinem eigenen Bereich!");
+					//drive through the own space (simulated through sleeping for the time the train would take to pass trough the own space)
 					Thread.sleep((lengthWithoutSharedSpace / velocityTrain2) * 1000);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
@@ -69,7 +85,7 @@ public class PrivateTrainRunner extends Thread {
 		
 		ArrayList<PrivateTrainRunner> threads = new ArrayList<>();
 		
-		//Erstelle und starte Threads
+		//Create and start tests
 		for(int i = 0; i < 2; i++) {
 			PrivateTrainRunner thread = new PrivateTrainRunner(i);
 			threads.add(thread);
